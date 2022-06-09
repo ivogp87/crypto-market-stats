4178e6935adc9ff3f89b5fdda1f0df9680c2a0c7
ivogp87 <ivaylopantaleev87@gmail.com>
2022-06-09 16:40:10 +0300

    Feature: implement share and rate us links

--- components/AboutUs/AboutUs.js
+++ components/AboutUs/AboutUs.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { View, Platform } from 'react-native';
+import { View, Platform, Share } from 'react-native';
 
 import themedStyles from './styles';
 import { useStyles } from '../../hooks';
 
+import AppText from '../AppText';
 import Heading from '../Heading';
 import Link from '../Link';
 
 const AboutUs = () => {
   const styles = useStyles(themedStyles);
+
+  const handleShare = async () => {
+    try {
+      await Share.share({
+        title: 'Crypto app recommendation',
+        message:
+          'Install CryptoMarketStats - free crypto price tracker: https://play.google.com/store/apps/details?id=net.cryptomarketstats',
+      });
+      // eslint-disable-next-line no-empty
+    } catch (_error) {}
+  };
 
   return (
     <>
@@ -26,6 +38,17 @@ const AboutUs = () => {
         >
           Contact Us
         </Link>
+        <Link
+          href="market://details?id=net.cryptomarketstats"
+          target="browser"
+          size="large"
+          style={styles.link}
+        >
+          Rate Us
+        </Link>
+        <AppText color="info" size="large" style={styles.link} onPress={handleShare}>
+          Share App
+        </AppText>
         <Link
           href="https://cryptomarketstats.net/privacy-policy.html"
           size="large"
