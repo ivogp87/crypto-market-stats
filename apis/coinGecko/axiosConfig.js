671857fc3231e83dba83399d0716713528f430c7
ivogp87 <ivaylopantaleev87@gmail.com>
2023-12-23 15:55:12 +0200

    Chore: add CoinGecko API key

--- apis/coinGecko/axiosConfig.js
+++ apis/coinGecko/axiosConfig.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://api.coingecko.com/api/v3';
+const API_KEY = process.env.EXPO_PUBLIC_COINGECKO_API_KEY;
 
 const coinGecko = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
+  headers: {
+    'x-cg-demo-api-key': API_KEY,
+  },
 });
 
 export default coinGecko;
