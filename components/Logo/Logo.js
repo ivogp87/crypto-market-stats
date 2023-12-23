1e30c294f3032555937f5bb711e678200d4fb8f0
ivogp87 <ivaylopantaleev87@gmail.com>
2023-12-23 16:57:02 +0200

    Chore: add expo-image ad delete CachedImage

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,38 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet } from 'react-native';
+import { Image } from 'expo-image';
 
 import { iconSize } from '../../styles';
 import { stylePropTypes } from '../../utils';
 
-import CachedImage from '../CachedImage';
+const placeholder =
+  'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9ab8d6934b83a4aa8ae5e8711609a70ca0ab1b2b/128/color/generic.png';
 
-const Logo = ({ url, id, size, style }) => {
+const Logo = ({ url, size, style }) => {
   const dimensions = iconSize[size] || iconSize.medium;
-  const imageUrl =
-    url ||
-    'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@9ab8d6934b83a4aa8ae5e8711609a70ca0ab1b2b/128/color/generic.png';
 
-  return (
-    <CachedImage
-      url={imageUrl}
-      cacheKey={`${url ? id : 'generic'}-logo`}
-      style={StyleSheet.compose(
-        {
-          width: dimensions,
-          height: dimensions,
-        },
-        style
-      )}
-    />
+  const styles = StyleSheet.compose(
+    {
+      width: dimensions,
+      height: dimensions,
+    },
+    style
   );
+
+  return <Image source={url} placeholder={placeholder} style={styles} />;
 };
 
 Logo.propTypes = {
   url: PropTypes.string,
   size: PropTypes.oneOf(['small', 'medium', 'large']),
   style: stylePropTypes,
-  id: PropTypes.string.isRequired,
 };
 
 Logo.defaultProps = {
